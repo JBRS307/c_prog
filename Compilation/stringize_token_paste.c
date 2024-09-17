0d3b3b40031fc6b09d9a98e7e092a09936276cf4
JBRS307 <kuba.rekas.307@gmail.com>
2024-09-17 17:22:57 +0200

    stringize and token pasting macros done

---
+++ Compilation/stringize_token_paste.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define concat(a, b) a ## b
+#define to_str(str) #str
+
+int main() {
+    int number = 25;
+    printf("Concatenated values: %d\n", concat(num, ber));
+    printf(to_str(Random text here));
+    putchar('\n');
+
+    return EXIT_SUCCESS;
+}
