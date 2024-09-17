7aebc8e2119c75555b1aab2a201ab78658bb4738
JBRS307 <kuba.rekas.307@gmail.com>
2024-09-17 16:00:06 +0200

    null assertions done

--- Compilation/null_assert.c
+++ Compilation/null_assert.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 double arr_avg(int* arr, size_t n) {
     assert(arr != NULL && "Pointer arr cannot be null");
-    // TODO
+    
+    int sum = 0;
+    for (size_t i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return (double)sum / n;
+
 }
 
 int main() {
-    // TODO
+    size_t n;
+    printf("How many elements in array: ");
+    scanf("%lu", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    printf("Enter array elements:\n");
+    for (size_t i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Average of elements: %lf\n", arr_avg(arr, n));
 
     return EXIT_SUCCESS;
 }
