cac1bc40b2ffaa961d41c47beaaa923f43feeaa3
JBRS307 <kuba.rekas.307@gmail.com>
2024-09-17 12:39:37 +0200

    null assertions code started

---
+++ Compilation/null_assert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double arr_avg(int* arr, size_t n) {
+    assert(arr != NULL && "Pointer arr cannot be null");
+    // TODO
+}
+
+int main() {
+    // TODO
+
+    return EXIT_SUCCESS;
+}
